99820e04b38723944a05b271ee8faf311c03b72b
Granwyn Tan <gran5g2017@gmail.com>
2025-04-03 05:39:54 +0000

    Clean the repository before syncing new files

--- js/index.js
+++
@@ -1,7 +0,0 @@
-if (sessionStorage.getItem("pointsRedeemed") === "true") {
-    console.log("Redirecting to redeemed.html...");
-    window.location.href = "redeemed.html"; // Ensure this page exists
-} else if (sessionStorage.getItem("userLoggedIn") == "true") {
-    console.log("Redirecting to stampcard.html...");
-    window.location.href = "stampcard.html"; // Ensure this page exists
-}

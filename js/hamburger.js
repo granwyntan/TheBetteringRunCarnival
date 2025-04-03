8a6567e59ba9f7de0a056369a81f25ecd4f70267
Granwyn Tan <gran5g2017@gmail.com>
2025-04-03 17:04:52 +0000

    Clean the repository before syncing new files

--- js/hamburger.js
+++
@@ -1,7 +0,0 @@
-const button = document.getElementById("hamburger-button");
-const hamburger = document.getElementById("hamburger");
-
-button.addEventListener("click", () => {
-    button.classList.toggle("active");
-    hamburger.classList.toggle("active");
-});

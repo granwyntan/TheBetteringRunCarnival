6d0a1762de4e1fccecce465651b5cf9551199c9b
Granwyn Tan <gran5g2017@gmail.com>
2025-04-04 14:15:51 +0000

    Sync new files from BytePenguinCode/TheBetteringRunCarnival

---
+++ js/index.js
@@ -0,0 +1,4 @@
+if (sessionStorage.getItem("pointsRedeemed") === "true") {
+    console.log("Redirecting to redeemed.html...");
+    window.location.href = "redeemed.html"; // Ensure this page exists
+}
